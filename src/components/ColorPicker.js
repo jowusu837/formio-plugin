1818b84938a96c6526d1e679b6b5c28b46d63256
Victor Owusu <vowusu@angeiongroup.com>
2025-04-18 13:48:43 +0000

    Update ColorPickerComponent render

--- src/components/ColorPicker.js
+++ src/components/ColorPicker.js
@@ -2,46 +2,51 @@ import { Components } from 'formiojs';
 const FieldComponent = Components.components.field;
 
 class ColorPickerComponent extends FieldComponent {
-  static schema(...extend) {
-    return FieldComponent.schema({
-      type: 'colorpicker',
-      label: 'Color Picker',
-      key: 'colorpicker',
-      inputType: 'color',
-      ...extend,
-    });
-  }
+    static schema(...extend) {
+        return FieldComponent.schema({
+            type: 'colorpicker',
+            label: 'Color Picker',
+            key: 'colorpicker',
+            inputType: 'color',
+            ...extend,
+        });
+    }
 
-  static get builderInfo() {
-    return {
-      title: 'Color Picker',
-      group: 'basic',
-      icon: 'paint-brush',
-      weight: 70,
-      schema: ColorPickerComponent.schema()
-    };
-  }
+    static get builderInfo() {
+        return {
+            title: 'Color Picker',
+            group: 'basic',
+            icon: 'paint-brush',
+            weight: 70,
+            schema: ColorPickerComponent.schema()
+        };
+    }
 
-  get defaultSchema() {
-    return ColorPickerComponent.schema();
-  }
+    get defaultSchema() {
+        return ColorPickerComponent.schema();
+    }
 
-  render() {
-    return super.render(this.renderTemplate('input', {
-      input: {
-        type: 'color',
-        value: this.dataValue
-      }
-    }));
-  }
+    render() {
+        return super.render(this.renderTemplate('input', {
+            input: {
+                type: 'input',
+                ref: this.component.key,
+                attr: {
+                    id: this.component.key,
+                    class: 'form-control',
+                    type: 'color',
+                }
+            }
+        }));
+    }
 
-  attach(element) {
-    const input = element.querySelector('input[type="color"]');
-    this.addEventListener(input, 'change', (event) => {
-      this.setValue(event.target.value);
-    });
-    return super.attach(element);
-  }
+    attach(element) {
+        const input = element.querySelector('input[type="color"]');
+        this.addEventListener(input, 'change', (event) => {
+            this.setValue(event.target.value);
+        });
+        return super.attach(element);
+    }
 }
 
 export default ColorPickerComponent;
